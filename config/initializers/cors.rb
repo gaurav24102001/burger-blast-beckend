fa93a266dc8e873e85dde3d1744501eec9837faa
gaurav singhal <singhalgaurav2001@gmail.com>
2022-12-07 13:31:32 +0530

    add cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,12 +2,16 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3001'
-  
+      origins 'https://burgers-blast.netlify.app'
       resource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head],
         credentials: true
     end
+    allow do
+      origins 'http://localhost:3001'
+      resource '/api/*', headers: :any, methods: [:get, :post, :options, :put, :delete],
+      credentials: true
+    end
   end
-  
+  Y
